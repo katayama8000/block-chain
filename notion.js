b487476de20e7f203add63c75db1b4d4220b0a60
katayama8000 <tattu.0310@gmail.com>
2023-01-03 23:50:48 +0900

    feat: not working

--- notion.js
+++ notion.js
@@ -1,16 +1,20 @@
 "use strict";
 console.log("Hello from notion.js")
 
-const token = "secret_kKwrMdx9jb2zVcrWqNV8Ur7iO9ML8r1i1EeylEHfy4a"
-const databaseId = "7f095051bb9c42c99aab88a7798f702d"
+const token = "secret_cb5N7mQAjQqOyVWRWIeoDZhExfHgrYOW4mcfv3Yk9gE"
+
 
 const { Client } = require('@notionhq/client');
 const notion = new Client({ auth: token });
 (async () => {
-  // const data = await notion.databases.query({ database_id: databaseId });
-  // console.log(data);
+  //https://www.notion.so/test-f9fb60e00bbd4f8e9a64b1d086cb4fe0
+  const databaseId = "f9fb60e00bbd4f8e9a64b1d086cb4fe0"
+  const length = databaseId.length
+  console.log(length)
+  const data = await notion.databases.query({ database_id: databaseId });
+  console.log(data);
   // const listUsersResponse = await notion.users.list({})
   // console.log(listUsersResponse)
-  const response = await notion.databases.retrieve({ database_id: databaseId });
-  console.log(response);
+  // const response = await notion.databases.retrieve({ database_id: databaseId });
+  // console.log(response);
 })();
